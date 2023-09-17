73b28964c1af8cfde0fc5a0d321a3c56091bd54e
Mikhail Dunaev <mih.dunaev@gmail.com>
2023-09-17 23:36:22 +0200

    Improve checking

--- healthCheck.go
+++ healthCheck.go
@@ -10,7 +10,17 @@ func startHealthCheck(healthCheckPort string, proxyPort string) {
 	log.Printf("Start listening healthcheck on port %s\n", healthCheckPort)
 	addr := ":" + healthCheckPort
 	err := http.ListenAndServe(addr, http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-		_, err := writer.Write([]byte(healthCheck(proxyPort)))
+		var header int
+		var textResponse string
+		if healthCheck(proxyPort) {
+			header = http.StatusOK
+			textResponse = "OK"
+		} else {
+			header = http.StatusServiceUnavailable
+			textResponse = "FAIL"
+		}
+		writer.WriteHeader(header)
+		_, err := writer.Write([]byte(textResponse))
 		if err != nil {
 			log.Printf("Error while writing healthcheck response: %v", err)
 		}
@@ -20,13 +30,13 @@ func startHealthCheck(healthCheckPort string, proxyPort string) {
 	}
 }
 
-func healthCheck(port string) string {
+func healthCheck(port string) bool {
 	curl := exec.Command("curl", "--socks5", "localhost:"+port, "https://ifcfg.co", "-m", "2")
 	err := curl.Run()
 	if err != nil {
 		log.Printf("Error while running curl: %v", err)
-		return "FAIL"
+		return false
 	} else {
-		return "OK"
+		return true
 	}
 }
